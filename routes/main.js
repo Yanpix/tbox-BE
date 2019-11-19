438bc9f8c3bc468591874e9547c5ea28b6126e0a
Kostenko V <volodymyr.kostenko@yanpix.com>
2019-11-19 12:15:04 +0200

    fix conflict

--- routes/main.js
+++ routes/main.js
@@ -5,12 +5,10 @@ const router = express.Router();
 
 router.use('/users', require('./users.js'));
 router.use('/tasks', require('./tasks.js'));
-<<<<<<< HEAD
 router.use('/parse', require('./parse.js'));
-=======
 router.use('/photos', require('./photos.js'));
 router.use('/news', require('./news.js'));
->>>>>>> master
+
 
 
 module.exports = router;
