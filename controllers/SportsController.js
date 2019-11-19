eef2a48d8a6ad467154ccd6dead3ea343e4d5d51
Kostenko V <volodymyr.kostenko@yanpix.com>
2019-11-19 15:22:27 +0200

    add query

--- controllers/SportsController.js
+++ controllers/SportsController.js
@@ -4,7 +4,12 @@ const bodyParser = require('body-parser');
 exports.getLosers = async function (req, res) {
 
     let team = req.body.team;
-    return res.json(team);
+    let matches = await Match.find({$or: [{homeTeam: "Juventus", isDraw:false, isHomeWinner:true}, {awayTeam: "Juventus",isDraw:false,isHomeWinner:false}]},{homeTeam:1, awayTeam:1})
+        .catch(err => console.log(err.message));
+
+
+
+    return res.json(matches);
     /*let models = await Match.find()
         .catch(err => console.log(err.message))
     return models ? res.json(models) : [];*/
