6dc83c81534f38d90c62f109f1b306d738c2f9fe
Kostenko V <volodymyr.kostenko@gmail.com>
2019-11-19 17:41:48 +0200

    finish

--- controllers/SportsController.js
+++ controllers/SportsController.js
@@ -1,16 +1,25 @@
 const Match = require('./../models/Match');
 const bodyParser = require('body-parser');
 
+function getOpponents(team, arr){
+    let opponents = [];
+    for(let item of arr){
+        if (item.homeTeam!=team)
+            opponents.push(item.homeTeam);
+        if (item.awayTeam!=team)
+            opponents.push(item.awayTeam);
+    }
+    return opponents;
+}
+
 exports.getLosers = async function (req, res) {
 
     let team = req.body.team;
-    let matches = await Match.find({$or: [{homeTeam: "Juventus", isDraw:false, isHomeWinner:true}, {awayTeam: "Juventus",isDraw:false,isHomeWinner:false}]},{homeTeam:1, awayTeam:1})
+    let matches = await Match.find(
+        {$or: [{homeTeam: team, isDraw:false, isHomeWinner:true},
+        {awayTeam: team,isDraw:false,isHomeWinner:false}]},{homeTeam:1, awayTeam:1})
         .catch(err => console.log(err.message));
+    let opponents = await getOpponents(team, matches);
 
-
-
-    return res.json(matches);
-    /*let models = await Match.find()
-        .catch(err => console.log(err.message))
-    return models ? res.json(models) : [];*/
+    return matches ? res.json(Array.from(new Set(opponents))) : [];
 };
